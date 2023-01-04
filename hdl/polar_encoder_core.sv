6835f0b02e9da2d6f35b83890e95bf93ac50b112
Wheberth Dias <wheberth@gmail.com>
2023-01-04 02:37:37 +0100

    Added initial combinational Polar encoder

--- hdl/polar_encoder_core.sv
+++ hdl/polar_encoder_core.sv
@@ -10,7 +10,33 @@ module polar_encoder_core #(
 );
 
 // Make sure N is power of 2
-n_is_power_of_two: assert #0 (N == (2**$clog2(N))) else $error("[%m]: N is not power of 2");
+initial n_is_power_of_two: assert (N == (2**$clog2(N))) else $error("[%m]: N is not power of 2");
+localparam NSTAGES = $clog2(N);
+
+// Bit storage for the stages
+genvar stage, bitn;
+wire [NSTAGES-1:0][N-1:0] data;
+
+// Initialize polar encoding mask
+logic [NSTAGES-1:0][N-1:0] mask;
+integer current_bit, current_stage;
+
+initial begin
+    for (current_bit=0; current_bit < N; current_bit++)
+        for (current_stage = 0; current_stage < NSTAGES; current_stage++) 
+            mask[current_stage][current_bit] = ~current_bit[current_stage];
+end
+
+for (stage = 0; stage < NSTAGES-1; stage++) begin
+    for (bitn = 0; bitn < N; bitn++)
+        assign data[stage+1][bitn] = (mask[stage][bitn]) ? 
+            (data[stage][bitn] ^ data[stage][bitn + (1<<stage)]) :
+            (data[stage][bitn]) ;
+end
+
+assign data[0] = in;
+assign out = data[NSTAGES-1];
+
 
 
 endmodule
--- tb/polar_encoder_core_tb.sv
+++ tb/polar_encoder_core_tb.sv
@@ -5,7 +5,7 @@ module polar_encoder_core_tb;
 
 localparam N = 32;
 
-logic [N-1:0] in = 0;
+logic [N-1:0] in = {32{1'b1}};
 logic [N-1:0] out;
 
 polar_encoder_core #(.N(N)) dut
@@ -15,7 +15,7 @@ polar_encoder_core #(.N(N)) dut
 );
 
 initial begin
-    #0
+    #10
     $stop();
 end
 
