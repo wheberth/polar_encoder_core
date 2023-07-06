112e526f0dc2158ad88dcc2855977cc3688f33a4
Wheberth Dias <wheberth@gmail.com>
2023-07-07 00:45:26 +0200

    Fix DISPLAY variable handling on docker Makefile targets

--- scripts/docker.mk
+++ scripts/docker.mk
@@ -2,18 +2,27 @@ DOCKER_RUN = podman run -it --userns=keep-id
 CONTAINER ?= vivado:2023.1
 BASEDIR ?= $(abspath .)
 WORK = $(shell basename $(BASEDIR))
-DOCKER_ENV_VARS += -e DISPLAY=$$DISPLAY
-DOCKER_ENV_VARS += -v $$HOME/.Xauthority:/home/$$USER/.Xauthority
 DOCKER_OPTS += --net=host
 MAKE_CMD ?= make
+DOCKER_ENV_VARS = 
+DOCKER_X_VARS =
 
-.PHONY: shell
+ifdef DISPLAY
+	DOCKER_X_VARS += -e DISPLAY=$$DISPLAY
+	DOCKER_X_VARS += -v $$HOME/.Xauthority:/home/$$USER/.Xauthority
+endif
 
-docker-%:
+.PHONY: shell check-display
+
+check-display:
+ifdef DISPLAY
 	@xhost +local:all
+endif
+
+docker-%: check-display
 	@$(DOCKER_RUN) $(DOCKER_OPTS) \
 	-v $(BASEDIR):/home/work/$(WORK):z -w /home/work/$(WORK) \
-	$(DOCKER_ENV_VARS) $(CONTAINER) \
+	$(DOCKER_X_VARS) $(DOCKER_ENV_VARS) $(CONTAINER) \
 	-c "echo -e '\e[0;34mRunning inside container => $(CONTAINER) \e[0m\n'; $(MAKE_CMD) $(lastword $(subst -, ,$@))"
 
 shell:
